c7559e74bfe430f111cc4d2a93270e435b05dd9b
Davide Zavanella <davide.zavanella@in.mynet.it>
2026-01-09 16:25:21 +0100

    Remove release helper scripts and tests from main (archived in archive/removed-scripts-tests)

--- .scripts/check_and_run_release_v0_1_3.ps1
+++
@@ -1,8 +0,0 @@
-if ($env:GITHUB_TOKEN) {
-    Write-Output 'GITHUB_TOKEN=SET'
-    Write-Output '--- Eseguo lo script create_release_v0_1_3.ps1 ---'
-    & .\scripts\create_release_v0_1_3.ps1
-}
-else {
-    Write-Output 'GITHUB_TOKEN=NOT SET'
-}
